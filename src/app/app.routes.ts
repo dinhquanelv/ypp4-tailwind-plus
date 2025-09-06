735ab4c363a9896f5259c288e21f0cbfaca33395
Dinh Quan <quanvonguyendinh@gmail.com>
2025-09-07 01:12:45 +0700

    setup routes for product-overviews & product-quickviews

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,18 @@
 import { Routes } from '@angular/router';
-import { ProductOverviews } from './features/product-overviews/pages/product-overviews';
+import { MainLayout } from './layouts/main-layout/main-layout';
+import { Ecommerce } from './features/ecommerce/ecommerce';
+import { ProductOverviews } from './features/ecommerce/components/product-overviews/product-overviews';
+import { ProductQuickviews } from './features/ecommerce/components/product-quickviews/product-quickviews';
 
-export const routes: Routes = [{ path: 'product-overviews', component: ProductOverviews }];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      { path: '', component: Ecommerce },
+      { path: 'ecommerce', component: Ecommerce },
+      { path: 'ecommerce/components/product-overviews', component: ProductOverviews },
+      { path: 'ecommerce/components/product-quickviews', component: ProductQuickviews },
+    ],
+  },
+];
