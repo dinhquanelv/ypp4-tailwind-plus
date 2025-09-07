49049bf96e854d49cb357fa08b8f5842b748e99d
Dinh Quan <quanvonguyendinh@gmail.com>
2025-09-07 20:54:59 +0700

    setup layout for product overviews module

--- src/app/features/ecommerce/components/product-overviews/product-overviews.ts
+++ src/app/features/ecommerce/components/product-overviews/product-overviews.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { WithImageGrid } from './sections/with-image-grid/with-image-grid';
+import { WithImageGallery } from './sections/with-image-gallery/with-image-gallery';
+import { WithTieredImages } from './sections/with-tiered-images/with-tiered-images';
+import { SplitWithImage } from './sections/split-with-image/split-with-image';
+import { WithTabs } from './sections/with-tabs/with-tabs';
 
 @Component({
   selector: 'app-product-overviews',
-  imports: [],
+  imports: [WithImageGrid, WithTieredImages, WithImageGallery, SplitWithImage, WithTabs],
   templateUrl: './product-overviews.html',
-  styles: ``
 })
-export class ProductOverviews {
-
-}
+export class ProductOverviews {}
---
+++ src/app/features/ecommerce/components/product-overviews/sections/split-with-image/split-with-image.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-split-with-image',
+  imports: [],
+  templateUrl: './split-with-image.html',
+})
+export class SplitWithImage {}
