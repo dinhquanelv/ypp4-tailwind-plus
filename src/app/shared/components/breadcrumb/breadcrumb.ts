215bda13044ddedacd6da654225d1aa51b1de05e
Dinh Quan <quanvonguyendinh@gmail.com>
2025-09-07 17:01:46 +0700

    add padding breadcrumb

--- src/app/shared/components/breadcrumb/breadcrumb.ts
+++ src/app/shared/components/breadcrumb/breadcrumb.ts
@@ -1,10 +1,9 @@
 import { Component, Input } from '@angular/core';
-import { BreadcrumbModule } from 'primeng/breadcrumb';
 import { MenuItemData } from '../../types/MenuItemData';
 
 @Component({
   selector: 'app-breadcrumb',
-  imports: [BreadcrumbModule],
+  imports: [],
   templateUrl: './breadcrumb.html',
 })
 export class Breadcrumb {
