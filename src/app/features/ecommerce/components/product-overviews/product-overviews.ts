aff76d840b5a6c771fe2b79fd512c17c5fbca101
Dinh Quan <quanvonguyendinh@gmail.com>
2025-09-07 23:43:51 +0700

    add section header

--- src/app/features/ecommerce/components/product-overviews/product-overviews.ts
+++ src/app/features/ecommerce/components/product-overviews/product-overviews.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { WithImageGrid } from './sections/with-image-grid/with-image-grid';
-import { WithImageGallery } from './sections/with-image-gallery/with-image-gallery';
-import { WithTieredImages } from './sections/with-tiered-images/with-tiered-images';
-import { SplitWithImage } from './sections/split-with-image/split-with-image';
-import { WithTabs } from './sections/with-tabs/with-tabs';
+import { SplitWithImage } from '@app/features/ecommerce/components/product-overviews/sections/split-with-image/split-with-image';
+import { WithImageGallery } from '@app/features/ecommerce/components/product-overviews/sections/with-image-gallery/with-image-gallery';
+import { WithImageGrid } from '@app/features/ecommerce/components/product-overviews/sections/with-image-grid/with-image-grid';
+import { WithTabs } from '@app/features/ecommerce/components/product-overviews/sections/with-tabs/with-tabs';
+import { WithTieredImages } from '@app/features/ecommerce/components/product-overviews/sections/with-tiered-images/with-tiered-images';
+import { SectionHeader } from '@app/shared/components/section-header/section-header';
 
 @Component({
   selector: 'app-product-overviews',
-  imports: [WithImageGrid, WithTieredImages, WithImageGallery, SplitWithImage, WithTabs],
+  imports: [
+    WithImageGrid,
+    WithTieredImages,
+    WithImageGallery,
+    SplitWithImage,
+    WithTabs,
+    SectionHeader,
+  ],
   templateUrl: './product-overviews.html',
 })
 export class ProductOverviews {}
